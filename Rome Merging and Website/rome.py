bd4b530b3bd0fb4585f94bdfcc95c51652696ee2
root <root@PC-US-UJVCHE.ad.adonms.com>
2019-08-16 16:02:50 -0400

    changed css and rome.py

--- Rome Merging and Website/rome.py
+++ Rome Merging and Website/rome.py
@@ -9,7 +9,7 @@ from influxdb import DataFrameClient
 import datetime
 
 client = InfluxDBClient(host="eu1-cnws001.int.mblox.com")
-clientd = InfluxDBClient(host="eu1-cnws001.int.mblox.com")
+clientd = DataFrameClient(host="eu1-cnws001.int.mblox.com")
 
 filesuc = ""
 filecomp = ""
@@ -40,14 +40,23 @@ def main(argv):
 if __name__ == "__main__":
     filesuc,filecomp = main(sys.argv[1:])
     
+dfgraf = p.read_csv(filesuc, header=0, delimiter=';', parse_dates = ['Time'])
+print(dfgraf.columns)
+dfcomp = p.read_csv(filecomp, header=0, sep=',', parse_dates = ['_time'], engine='python')
 
-dfgraf = p.read_csv(filesuc, header=0, sep=';', parse_dates = ['Time'])
-dfcomp = p.read_csv(filecomp, header=0, parse_dates = ['_time'])
+#TESTING
+print(type(dfgraf['DR'][0]))
+dfgraf['DR'].replace('-', '0', inplace=True)
+dfgraf['DR'] = p.to_numeric(dfgraf['DR'])
+print(type(dfgraf['DR']))
+dfgraf['Successful'].replace('-', '0', inplace=True)
+dfgraf['Successful'] = p.to_numeric(dfgraf['Successful'])
 
 print('SUCCESS%')
 dfgraf.rename(columns={"country": "Country"}, inplace=True)
 dfgraf['success%'].replace('-', '0', inplace=True)
 dfgraf['success%'] = p.to_numeric(dfgraf['success%'])
+dfgraf.rename(columns={'success%': 'SuccessRate'}, inplace=True)
 print(dfgraf.head())
 
 print("")
@@ -60,9 +69,38 @@ print("")
 
 print('MERGED')
 dfmerge = p.merge(dfgraf, dfcomp, on=['Time','Country'])
-dfmerge['C-S'] = abs(dfmerge['CompletionRate'] - dfmerge['success%'])
-dfmerge['Time' = p.to_datetime(dfmerge['Time'], format='%Y-%m-%d %H:%M:%S.%f')
+dfmerge['C-S'] = abs(dfmerge['CompletionRate'] - dfmerge['SuccessRate'])
+dfmerge['Time'] = p.to_datetime(dfmerge['Time'], format='%Y-%m-%d %H:%M:%S.%f')
 print(dfmerge.head())
+print(dfmerge.columns)
 
 dfmerge = dfmerge.set_index('Time')
-clientd.write_points(dfmerge, 'merged', protocol='json', database='rome')
+# dfmerge = dfmerge.reindex(columns=dfmerge['Country'])
+
+ctrycol = dfmerge['Country']
+succol = dfmerge['SuccessRate']
+compcol = dfmerge['CompletionRate']
+diffcol = dfmerge['C-S']
+
+srdf = p.DataFrame({'SuccessRate':succol, 'Country':ctrycol})
+srtags = dict(sorted(srdf.values.tolist()))
+print(srdf.head())
+
+crdf = p.DataFrame({'Country':ctrycol, 'CompletionRate':compcol})
+crtags = dict(sorted(crdf.values.tolist()))
+
+diffdf = p.DataFrame({'Country':ctrycol, 'Difference':diffcol})
+difftags = dict(sorted(diffdf.values.tolist()))
+
+statsdf = p.DataFrame({'Country':ctrycol, 'MT':dfmerge['MT'], 'Supplier':dfmerge['Supplier'], 'supplier%':dfmerge['supplier%'], 'DR':dfmerge['DR'], 'Successful':dfmerge['Successful'], 'SuccessRate':succol, 'SMSSent':dfmerge['SMSSent'], 'SMSValidated':dfmerge['SMSValidated'], 'CompletionRate':compcol, 'Difference':diffcol})
+#stags = p.Series(dfmerge.CompletionRate.values, index=dfmerge.Country).to_dict()
+#ctags = {dfmerge['Country']:dfmerge['CompletionRate']}
+#dtags = {dfmerge['Country']:dfmerge['C-S']}
+#srdf = dfmerge.set_index('Country')
+
+tags = {'Country':srdf[['Country']]}
+sucdf = p.DataFrame({'SuccessRate':succol})
+clientd.write_points(statsdf, 'totalstats', tag_columns=['Country'], protocol='json', database='rome', batch_size=1000)
+#clientd.write_points(crdf, 'crate', tags=crtags, protocol='json', database='rome')
+#clientd.write_points(diffdf, 'diff', tags=difftags, protocol='json', database='rome')
+
